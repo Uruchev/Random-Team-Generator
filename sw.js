6a0a76064c5c71d7ab8ba7cdef44d8813deed68a
Uruchev <ventislav.uruchev@gmail.com>
2025-11-04 13:04:12 +0200

    chore(sw): bump cache version to refresh Pages and fetch new CSS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿// Service Worker – Precache app shell + runtime cache for CDNs
-const VERSION = 'v1.1.0';
+// Service Worker � Precache app shell + runtime cache for CDNs
+const VERSION = 'v1.2.0';
 const APP_SHELL = [
   './',
   './index.html',
